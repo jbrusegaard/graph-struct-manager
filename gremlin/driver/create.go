35360818a0b89cc98c3d516563cf13c683cd309d
jbrusegaard <jbrusegaard@gmail.com>
2026-01-12 23:37:53 -0600

    refactor to support neptune

--- gremlin/driver/create.go
+++ gremlin/driver/create.go
@@ -1,6 +1,7 @@
 package driver
 
 import (
+	"errors"
 	"reflect"
 	"time"
 
@@ -13,44 +14,36 @@ func Create[T gsmtypes.VertexType](db *GremlinDriver, value *T) error {
 }
 
 func Update[T gsmtypes.VertexType](db *GremlinDriver, value *T) error {
-	return createOrUpdate(db, value)
+	return updateVertex(db, value)
 }
 
-func createOrUpdate[T gsmtypes.VertexType](db *GremlinDriver, value *T) error {
+func updateVertex[T gsmtypes.VertexType](db *GremlinDriver, value *T) error {
 	err := validateStructPointerWithAnonymousVertex(value)
 	if err != nil {
 		db.logger.Errorf("Validation failed: %v", err)
 		return err
 	}
 	now := time.Now().UTC()
-	label, mapValue, err := structToMap(value)
+	_, mapValue, err := structToMap(value)
 	if err != nil {
 		return err
+	}
+	if mapValue["id"] == nil {
+		return errors.New(
+			"invalid update operation value does not contain id field or id was not set",
+		)
 	}
 	id := mapValue["id"]
 	delete(mapValue, "id")
 	mapValue[gsmtypes.LastModified] = now
-	var query *gremlingo.GraphTraversal
-	newMap := make(map[any]any, len(mapValue))
-	if id == nil {
-		mapValue[gsmtypes.CreatedAt] = now
-		for k, v := range mapValue {
-			newMap[k] = v
-		}
-		newMap[gremlingo.T.Label] = label
-		query = db.g.MergeV(newMap)
-	} else {
-		query = db.g.MergeV(map[any]any{gremlingo.T.Id: id})
-	}
-	query.Option(gremlingo.Merge.OnMatch, mapValue)
-	vertexID, err := query.Id().Next()
+	query := db.g.V(id)
+	query = handlePropertyUpdate(db, mapValue, query)
+	_, err = query.Next()
 	if err != nil {
 		return err
 	}
-	reflect.ValueOf(value).Elem().FieldByName("ID").Set(reflect.ValueOf(vertexID.GetInterface()))
 	reflectNow := reflect.ValueOf(now)
 	reflect.ValueOf(value).Elem().FieldByName("LastModified").Set(reflectNow)
-	reflect.ValueOf(value).Elem().FieldByName("CreatedAt").Set(reflectNow)
 	return nil
 }
 
@@ -69,7 +62,22 @@ func createVertex[T gsmtypes.VertexType](db *GremlinDriver, value *T) error {
 	mapValue[gsmtypes.LastModified] = now
 	mapValue[gsmtypes.CreatedAt] = now
 	query := db.g.AddV(label)
-	for k, v := range mapValue {
+	query = handlePropertyUpdate(db, mapValue, query)
+	vertexID, err := query.Id().Next()
+	if err != nil {
+		return err
+	}
+	reflect.ValueOf(value).Elem().FieldByName("ID").Set(reflect.ValueOf(vertexID.GetInterface()))
+	reflectNow := reflect.ValueOf(now)
+	reflect.ValueOf(value).Elem().FieldByName("LastModified").Set(reflectNow)
+	reflect.ValueOf(value).Elem().FieldByName("CreatedAt").Set(reflectNow)
+	return nil
+}
+
+func handlePropertyUpdate(
+	db *GremlinDriver, properties map[string]any, query *gremlingo.GraphTraversal,
+) *gremlingo.GraphTraversal {
+	for k, v := range properties {
 		// check if v is a slice and Neptune is being used
 		if db.dbDriver == Neptune &&
 			(reflect.ValueOf(v).Kind() == reflect.Slice || reflect.ValueOf(v).Kind() == reflect.Map) {
@@ -90,13 +98,5 @@ func createVertex[T gsmtypes.VertexType](db *GremlinDriver, value *T) error {
 			query = query.Property(gremlingo.Cardinality.Single, k, v)
 		}
 	}
-	vertexID, err := query.Id().Next()
-	if err != nil {
-		return err
-	}
-	reflect.ValueOf(value).Elem().FieldByName("ID").Set(reflect.ValueOf(vertexID.GetInterface()))
-	reflectNow := reflect.ValueOf(now)
-	reflect.ValueOf(value).Elem().FieldByName("LastModified").Set(reflectNow)
-	reflect.ValueOf(value).Elem().FieldByName("CreatedAt").Set(reflectNow)
-	return nil
+	return query
 }
