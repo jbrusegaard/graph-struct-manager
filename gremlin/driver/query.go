5eadadf00ca1934a967e2eaaaf99b0bdd872db7d
jbrusegaard <jbrusegaard@gmail.com>
2026-01-31 10:35:29 -0600

    add fix for In/Without only working with []any

--- gremlin/driver/query.go
+++ gremlin/driver/query.go
@@ -600,7 +600,7 @@ func (q *Query[T]) doOrderSkipRange(query *gremlingo.GraphTraversal) *gremlingo.
 	return query
 }
 
-func (q *Query[T]) addQueryConditions(query *gremlingo.GraphTraversal) {
+func (q *Query[T]) addQueryConditions(query *gremlingo.GraphTraversal) { //nolint:gocognit
 	// Apply conditions
 	for _, condition := range q.conditions {
 		if condition.traversal != nil {
@@ -624,17 +624,24 @@ func (q *Query[T]) addQueryConditions(query *gremlingo.GraphTraversal) {
 			query = query.Has(condition.field, gremlingo.P.Lt(condition.value))
 		case comparator.LTE, "lte":
 			query = query.Has(condition.field, gremlingo.P.Lte(condition.value))
-		case comparator.IN:
-			if slice, ok := condition.value.([]any); ok {
-				query = query.Has(condition.field, gremlingo.P.Within(slice...))
-			}
 		case comparator.CONTAINS:
 			if strVal, ok := condition.value.(string); ok {
 				query = query.Has(condition.field, gremlingo.TextP.Containing(strVal))
 			}
-		case comparator.WITHOUT:
-			if slice, ok := condition.value.([]any); ok {
-				query = query.Has(condition.field, gremlingo.P.Without(slice...))
+		case comparator.IN, comparator.WITHOUT:
+			var sliceValue []any
+			value := reflect.ValueOf(condition.value)
+			if value.IsValid() && value.Kind() == reflect.Slice {
+				for i := range value.Len() {
+					sliceValue = append(sliceValue, value.Index(i).Interface())
+				}
+			} else {
+				sliceValue = append(sliceValue, condition.value)
+			}
+			if comparator.WITHOUT == condition.operator {
+				query = query.Has(condition.field, gremlingo.P.Without(sliceValue))
+			} else {
+				query = query.Has(condition.field, gremlingo.P.Within(sliceValue))
 			}
 		}
 	}
